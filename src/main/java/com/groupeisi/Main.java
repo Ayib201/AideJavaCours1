aed11c0aecd84a2c37e13d1e4ecbd2919cca08c8
Ayib201 <ayibt2612@gmail.com>
2025-12-19 20:33:44 +0000

    Fin de session

--- src/main/java/com/groupeisi/Main.java
+++ src/main/java/com/groupeisi/Main.java
@@ -1,17 +1,46 @@
 package com.groupeisi;
 
+import com.groupeisi.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<User> users = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Combien utilisateur voulez-vous entrer ?");
+        int number = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < number; i++) {
+            System.out.print("Please enter your name: \n");
+            String name = in.nextLine();
+            System.out.print("Saisir votre email \n");
+            String email = in.nextLine();
+            User user = new User(name, email);
+            users.add(user);
         }
+        for(User user : users){
+            System.out.println("Nom : " + user.getName()+ " Email : " + user.getEmail()+ '\n');
+        }
+//        User user = new User();
+//        System.out.print("Please enter your name: \n");
+//        String name = in.nextLine();
+//        user.setName(name);
+//        System.out.print("Voulez-vous entrez votre email \n");
+//        String response = in.nextLine();
+//        if (response.equalsIgnoreCase("oui")){
+//            System.out.print("Saisir votre email \n");
+//            String email = in.nextLine();
+//            user.setEmail(email);
+//            System.out.println("Nom :" + user.getName() + " email " + user.getEmail());
+//        }else {
+//            System.out.println(user.getName());
+//        }
+
     }
+
 }
